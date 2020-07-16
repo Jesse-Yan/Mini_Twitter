125ecea6a334369926435c58ac4fe0d151b85ab4
ChengpoYan <cyan46@wisc.edu>
2020-07-16 15:35:57 +0800

    .

--- src/index.js
+++ src/index.js
@@ -11,7 +11,6 @@ import authReducer from "./store/reducers/auth";
 import personReducer from "./store/reducers/personalData";
 import rootSaga from "./store/sagas/index";
 import App from "./App";
-import * as serviceWorker from "./serviceWorker";
 
 const composeEnhancers =
   process.env.NODE_ENV === "development"
@@ -41,8 +40,3 @@ ReactDOM.render(
   </Provider>,
   document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
